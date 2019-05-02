2c619bf749783d7e413f1385c0a66634dfc1e415
Bryan Apellanes <bapellanes@vimly.com>
2019-05-02 14:44:34 -0700

    command line args handling

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-var arg1 = process.argv[2];
-
 const { app, BrowserWindow } = require('electron');
-
+const fs = require('fs');
+const path = require('path');
+const args = require('./cli-args');
 
 var windowmanager = require('./WindowManager')(app, BrowserWindow);
 
 app.on('ready', () => {
-    windowmanager.start("tests");
-    if(arg1 == "dev") {
-        windowmanager.openDevTools("tests");
+    var page = 'tests';
+    if(args["page"]){
+        page = args["page"];
+    }
+    windowmanager.start(page);
+    if(args["dev"]) {
+        windowmanager.openDevTools(page);
     }
 })
 
