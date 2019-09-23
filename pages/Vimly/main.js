4d27cf8e18c28845e848357810400828ec6cbef3
Bryan Apellanes <bapellanes@vimly.com>
2019-09-23 15:02:05 -0700

    refactor/move files for require

--- pages/Vimly/main.js
+++ pages/Vimly/main.js
@@ -1,8 +1,12 @@
 // Admin
 
-window.qi = require('../../js/bam/data/qi.js');
-window.sdo = require('../../js/bam/data/sdo.js');
-window.dao = require('../../js/bam/data/dao.js');
-window.bam = require('../../js/bam/bam.js')({});
+// window.qi = require('../../js/bam/data/qi.js');
+// window.sdo = require('../../js/bam/data/sdo.js');
+// window.dao = require('../../js/bam/data/dao.js');
+// window.bam = require('../../js/bam/bam.js')({});
+
+//require('./_lib/js/bam-main.js')('../../../../js/bam');
+
+require('../../js/bam/main.js')('../../../../js/bam');
 
 window.vimly = require('./vimly.js');
